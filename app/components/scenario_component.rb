ffa3e3502ac52593c68446259f089aeeea655bb3
Fred Rocher <frocher2005@gmail.com>
2025-12-14 20:41:11 +0100

    Add Editable Placeholders and Update Scenario Component Logic
    - Introduced editable placeholders in the `ScenarioComponent` for the Given, When, and Then fields, enhancing user interaction.
    - Updated the component's JavaScript logic to handle text updates and trigger AJAX requests for saving changes.
    - Added new helper methods in `ScenarioComponent` to manage form IDs and text retrieval, improving code organization and readability.
    - Enhanced CSS styles for editable placeholders to align with a Notion-style interface, improving the overall user experience.

--- app/components/scenario_component.rb
+++ app/components/scenario_component.rb
@@ -5,6 +5,30 @@ class ScenarioComponent < ApplicationComponent
     @project = project
   end
 
+  def update_path
+    helpers.project_feature_scenario_path(@project, @feature, @scenario)
+  end
+
+  def form_id
+    "scenario-details-form-#{@scenario.id}"
+  end
+
+  def wrapper_id
+    "scenario-wrapper-#{@scenario.id}"
+  end
+
+  def given_text
+    @scenario.given.presence || ""
+  end
+
+  def when_text
+    @scenario[:when].presence || ""
+  end
+
+  def then_text
+    @scenario[:then].presence || ""
+  end
+
   private
 
   attr_reader :scenario, :feature, :project
