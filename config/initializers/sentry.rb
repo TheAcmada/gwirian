185f17f1a52d5e97fe58ca9acc2c2ea214ad982e
Fred Rocher <frocher2005@gmail.com>
2026-01-01 10:14:27 +0100

    Add traces_sample_rate configuration to Sentry initializer
    - Set traces_sample_rate to 0.1 in production and 1.0 in other environments for improved performance monitoring.

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -6,6 +6,8 @@ if ENV["SENTRY_DSN"].present?
     config.dsn = ENV["SENTRY_DSN"]
     config.breadcrumbs_logger = [ :active_support_logger, :http_logger ]
 
+    config.traces_sample_rate = Rails.env.production? ? 0.1 : 1.0
+
     # Add data like request headers and IP for users,
     # see https://docs.sentry.io/platforms/ruby/data-management/data-collected/ for more info
     config.send_default_pii = true
