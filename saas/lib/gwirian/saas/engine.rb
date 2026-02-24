9ff0fa2a1f4ba95e38965ee531f33991fe5b7967
Fred Rocher <frocher2005@gmail.com>
2026-02-24 23:19:22 +0100

    Remove commented-out route definitions in the Gwirian SaaS engine initialization to clean up the code and improve readability.

--- saas/lib/gwirian/saas/engine.rb
+++ saas/lib/gwirian/saas/engine.rb
@@ -4,17 +4,7 @@ module Gwirian
   module Saas
     class Engine < ::Rails::Engine
       initializer "gwirian.saas.routes", after: :add_routing_paths do |app|
-        # Routes that rely on the implicit account tenant should go here instead of in +routes.rb+.
         app.routes.prepend do
-          namespace :account do
-            resource :billing_portal, only: :show
-            resource :subscription do
-              scope module: :subscriptions do
-                resource :upgrade, only: :create
-                resource :downgrade, only: :create
-              end
-            end
-          end
         end
       end
 
